dc6416875aca2e404acd433c7cda3fe825ce6818
如果超人不会飞 <505300614@qq.com>
2015-07-30 11:15:03 +0800

    Update main.cpp

--- ConvertUtf8ToGb2312/main.cpp
+++ ConvertUtf8ToGb2312/main.cpp
@@ -5,7 +5,8 @@ int main()
 {
 	Configfile config("cpp,h");
 
-
+        
+        //不能包含中文目录，由于windows目录分隔符使用"\",在c语言系中文转义字符，可以将其改变"/",或者使用"\\".
 	Utf8toGb2312::GetInstance()->Conv_Utf8_files("D:/shuju", config, NULL, true);
 
 
